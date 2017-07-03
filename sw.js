61e85f2bd6e57a4136310fcacf9767338ed91e96
Tim Verheijke <t.verheijke@gmail.com>
2017-07-03 19:25:02 +0200

    add simple service worker

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'simple-diff-calculator-v1';
+const urlsToCache = [
+    '/',
+    '/styles/style.css',
+    '/scripts/index.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Installing', CACHE_NAME);
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                console.log('Matched cache entry', event.request.url, response);
+                return response ? response : fetch(event.request)
+            })
+    );
+});
